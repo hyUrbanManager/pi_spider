36710bdac57c3b324a230b68c058eff724d71a04
huangye <huangye@cvte.com>
2022-05-18 13:30:15 +0800

    [feature]增加定时观察wan ip，每小时

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 const schedule = require('node-schedule')
+const watcher = require('./wan_ip_watcher.js')
+const app = express()
 
 app.use(express.static('./'))
 
@@ -14,4 +15,10 @@ app.listen(8081, () => {
 //每5分钟执行一次
 schedule.scheduleJob('*/5 * * * *', () => {
     console.log('start check:' + new Date().toLocaleString())
+})
+
+// 每小时观察一次ip
+schedule.scheduleJob('0 * * * *', () => {
+    console.log('start check ip. ' + new Date().toLocaleString())
+    watcher.watch()
 })
