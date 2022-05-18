9e9e11d15ac6e8ed39001b570ac322dd68e7841e
huangye <huangye@cvte.com>
2022-05-18 13:36:18 +0800

    [feature]删除无用express

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-const express = require('express')
 const schedule = require('node-schedule')
 const watcher = require('./wan_ip_watcher.js')
-const app = express()
-
-app.use(express.static('./'))
-
-app.get('/', (req, res) => {
-})
-
-app.listen(8081, () => {
-    console.log('listening...')
-})
-
-//每5分钟执行一次
-schedule.scheduleJob('*/5 * * * *', () => {
-    console.log('start check:' + new Date().toLocaleString())
-})
 
 // 每小时观察一次ip
 schedule.scheduleJob('0 * * * *', () => {
