7a58df5f54f28768e4ea3e2a3d4bd376b5856261
huangye <huangye@cvte.com>
2022-05-18 13:28:06 +0800

    [feature]增加给message模块，发送消息给微信。增加观察ip变化模块

---
+++ message.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const apikey = 'SCT148567TRPdvRERqkYligdCzvgIOJ5at'
+const url = `https://sctapi.ftqq.com/${apikey}.send`
+
+module.exports = {
+    msg(title, body) {
+        axios.get(`${url}/?title=${title}&desp=${body}`)
+            .then(response => {
+                console.log(response.data.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+}
+
+if (require.main === module) {
+    module.exports.msg('test title', 'test body')
+}
---
+++ wan_ip_watcher.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const fs = require('fs');
+const message = require('./message.js');
+
+const wan_ip_url = 'http://httpbin.org/ip'
+
+function compare_cache_wan_ip(ip) {
+    file_name = './data/wan_ip.json'
+    exist = fs.existsSync(file_name)
+    if (exist) {
+        var data = fs.readFileSync(file_name)
+        if (data.ip == ip) {
+            return true
+        } else {
+            console.log(`ip change, from ${data.ip} to ${ip}`);
+            data.ip = ip
+            fs.writeFileSync(file_name, JSON.stringify(data))
+            return false
+        }
+    } else {
+        var data = {'ip': ip}
+        fs.writeFileSync(file_name, JSON.stringify(data))
+        return false
+    }
+}
+
+module.exports = {
+    watch() {
+        axios.get(wan_ip_url)
+            .then(response => {
+                wan_ip = response.data.origin;
+                console.log(`wan ip: ${wan_ip}`)
+                if (!compare_cache_wan_ip(wan_ip)) {
+                    message.msg(`ip change to ${wan_ip}`)
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+}
+
+if (require.main === module) {
+    module.exports.watch()
+}
